bdb4d5e1a813743c8609bf15e77cd5d28fc79783
asmcore <vs.sharp@mail.ru>
2021-10-21 21:07:32 +0300

    test commit

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ for i in range(len(less)):
             string[j] = x_str[j]
         print('  '+ lib.to_string(string)+' - error')
         break
-print('*'*50)
+print('*'*45)
 input()
 os.system('clear')
